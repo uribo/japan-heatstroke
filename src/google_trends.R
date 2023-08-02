3625d7a6820b89e1bd2835296cde368b4329e7ca
Shinya Uryu <suika1127@gmail.com>
2023-08-02 16:39:39 +0900

    [modify]Google trendsのデータを更新

--- src/google_trends.R
+++ src/google_trends.R
@@ -1,6 +1,6 @@
 ################################
 # Google Trends
-# 実行日: 2022-09-02
+# 実行日: 2023-08-02
 ################################
 library(dplyr)
 library(ggplot2)
@@ -8,16 +8,23 @@ x <-
   gtrendsR::gtrends(keyword = "熱中症",
                     geo = "JP",
                     time = "all")
-readr::write_rds(x, here::here("data/gtrends_20040101-20220801.rds"),
+rds_path <- 
+  glue::glue("data/gtrends_{day_min}-{day_max}.rds",
+             day_min = stringr::str_remove_all(as.character(min(x$interest_over_time$date)), "-"), 
+             day_max = stringr::str_remove_all(as.character(max(x$interest_over_time$date)), "-"))
+readr::write_rds(x, 
+                 here::here(rds_path),
                  compress = "xz")
-usethis::use_git_ignore("data/gtrends_20040101-20220801.rds")
+usethis::use_git_ignore(rds_path)
 
-# 2004-01-01 to 2022-08-01
+rds_path
+
+# 2004-01-01 to 2023-07-01
 x$interest_over_time$date %>% range()
 
 x$interest_over_time %>% 
   tibble::as_tibble() %>% 
-  filter(between(lubridate::year(date), 2017, 2019)) %>% 
+#  filter(between(lubridate::year(date), 2017, 2019)) %>% 
   mutate(hits = if_else(hits == "<1", "0.5", hits) %>% 
            as.numeric()) %>% 
   ggplot() +

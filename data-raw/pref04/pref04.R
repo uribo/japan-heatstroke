5e5aa54475bb4c7993fb70b1cb7a0623c09bdb9b
Shinya Uryu <suika1127@gmail.com>
2023-08-28 17:02:48 +0900

    Tweak

--- data-raw/pref04/pref04.R
+++ data-raw/pref04/pref04.R
@@ -1,1 +1,5 @@
-# https://www.pref.miyagi.jp/soshiki/syoubou/netyuusyou.html
+################################
+# 04. 宮城県
+# 消防本部別でない
+# # https://www.pref.miyagi.jp/soshiki/syoubou/netyuusyou.html
+################################
--- data-raw/pref23/pref23.R
+++ data-raw/pref23/pref23.R
@@ -1,1 +1,5 @@
+################################
+# 23. 愛知県
+# 消防本部別でない
+################################
 # https://www.pref.aichi.jp/soshiki/shobohoan/nettyuusyouhansou.html

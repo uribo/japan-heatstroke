636857b65580b045aa68c8a627e2ddb05a897af2
Shinya Uryu <r.sh1nya.uryu@gmail.com>
2023-09-05 00:11:53 +0900

    [modify]PDFへのリンク

--- data-raw/pref07/pref07.R
+++ data-raw/pref07/pref07.R
@@ -1,4 +1,34 @@
 ################################
 # 07. 福島県
 ################################
-# https://www.pref.fukushima.lg.jp/sec/16025a/necchu-02.html
+# 1. PDFのダウンロード -----------------------------------------------------------
+library(dplyr)
+library(rvest)
+x <-
+  read_html("https://www.pref.fukushima.lg.jp/sec/16025a/necchu-02.html")
+
+df <- 
+  tibble::tibble(
+  name = x |> 
+    html_nodes(css = "#main_body > div.detail_free > p > a") |> 
+    html_text(),
+  url = x |> 
+    html_nodes(css = "#main_body > div.detail_free > p > a") |> 
+    html_attr(name = "href") |> 
+    xml2::url_absolute(base = "https://www.pref.fukushima.lg.jp/")) |> 
+  ensurer::ensure(nrow(.) >= 34L) |>
+  filter(stringr::str_detect(name, "(平成|令和).+年")) |> 
+  ensurer::ensure(nrow(.) == 10L) |> 
+  mutate(name = stringr::str_squish(name) |> 
+           stringi::stri_trans_nfkc() |> 
+           stringr::str_extract(".+年") |> 
+           stringr::str_remove_all("[[:space:]]"))
+
+df |> 
+  dplyr::filter(stringr::str_detect(name, "月別", negate = TRUE)) |> 
+  purrr::pwalk(
+    function(url, name) {
+      download.file(url = url,
+                    destfile = here::here(glue::glue("data-raw/pref07/{name}.pdf")))
+    }
+  )
--- data-raw/pref21/pref21.R
+++ data-raw/pref21/pref21.R
@@ -1,4 +1,19 @@
 ################################
 # 21. 岐阜県
 ################################
-# https://www.pref.gifu.lg.jp/page/6398.html
+# 1. [WIP]PDFのダウンロード -----------------------------------------------------------
+library(dplyr)
+library(rvest)
+x <- 
+  read_html("https://www.pref.gifu.lg.jp/page/6398.html")
+
+x |> 
+  html_elements(css = "#main_body > div.detail_free") |> 
+  html_elements(css = "table > tbody > tr > td > a") |> 
+  html_attr(name = "href")
+
+x |> 
+  html_elements(css = "#main_body > div.detail_free") |> 
+  html_elements(css = "table > tbody > tr > td") |> 
+  html_text() |> 
+  stringr::str_subset("PDF")

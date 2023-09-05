21cd8f2a5038eabbb057aebb5ca0fe4950eb2c35
Shinya Uryu <suika1127@gmail.com>
2023-09-05 11:45:56 +0900

    [modify]PDFのダウンロード

--- data-raw/pref04/pref04.R
+++ data-raw/pref04/pref04.R
@@ -1,5 +1,35 @@
 ################################
 # 04. 宮城県
 # 消防本部別でない
-# # https://www.pref.miyagi.jp/soshiki/syoubou/netyuusyou.html
 ################################
+library(rvest)
+library(purrr)
+x <-
+  read_html("https://www.pref.miyagi.jp/soshiki/syoubou/netyuusyou.html")
+
+x2 <- 
+  x |> 
+  html_nodes(css = "#tmp_contents > ul > li > a")
+
+df <- 
+  tibble::tibble(
+  title = x2 |> 
+    html_text(),
+  url = x2 |> 
+    html_attr(name = "href")
+) |> 
+  dplyr::filter(stringr::str_detect(url, ".pdf$"))
+
+urls <- 
+  df |> 
+  dplyr::pull(url) |> 
+  xml2::url_absolute(base = "https://www.pref.miyagi.jp/")
+
+download_pdf <- 
+  slowly(function(x)
+    curl::curl_download(x,
+                        destfile = here::here("data-raw/pref04", basename(x))), 
+    rate = rate_delay(pause = 8), 
+    quiet = FALSE)
+urls |> 
+  purrr::walk(download_pdf)

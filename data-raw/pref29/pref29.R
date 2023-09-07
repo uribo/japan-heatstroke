5117603406c409bdde0ac8af39887dd257521453
Shinya Uryu <r.sh1nya.uryu@gmail.com>
2023-09-08 00:13:40 +0900

    Update

--- data-raw/pref29/pref29.R
+++ data-raw/pref29/pref29.R
@@ -1,4 +1,7 @@
 ################################
 # 29. 奈良県
+# 令和4年〜令和5年
+# https://www.pref.nara.jp/dd.aspx?moduleid=17530&pfromid=65#moduleid17530
+# https://www.pref.nara.jp/dd.aspx?moduleid=17530&pfromid=175#moduleid17530
 ################################
 # https://www.pref.nara.jp/dd.aspx?menuid=1625

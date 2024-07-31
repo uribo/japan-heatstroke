4652ed51bbb22c2bcbd3e7a6f513ab36bcd8911d
Shinya Uryu <suika1127@gmail.com>
2024-07-31 10:04:30 +0900

    [modify]特別警戒アラートを含む形式に対応

--- R/moe_alert.R
+++ R/moe_alert.R
@@ -1,5 +1,8 @@
 read_moe_alert <- memoise::memoise(
   function(year) {
+    if (dplyr::between(year, 2019, lubridate::year(lubridate::today())) == FALSE) {
+      rlang::abort("The data is available only after 2019 to the present.")
+    }
     if (lubridate::year(lubridate::today()) == year) {
       tgt_url <- 
         "https://www.wbgt.env.go.jp/alert_record.php"
@@ -16,24 +19,55 @@ read_moe_alert <- memoise::memoise(
   })
 
 alert_to_long <- function(df, year) {
+  alert_type_n <- 
+    colnames(df) |> 
+    stringr::str_count("発表回数") |> 
+    sum()
+  if (alert_type_n == 1) {
+    df <- 
+      df |> 
+      purrr::set_names(
+        c(names(df)[1:2],
+          paste0(
+            year,
+            "/",
+            names(df)[3:ncol(df)],
+            " ",
+            paste0(c(5, 17), ":00:00")
+          ))
+      ) |> 
+      dplyr::slice(-1) |>  
+      tidyr::pivot_longer(cols = contains("/"),
+                          names_to = "datetime",
+                          values_to = "alert") |> 
+      dplyr::select(-2) |> 
+      readr::type_convert("ccc")
+  } else if (alert_type_n == 2) {
+    df <- 
+      df |> 
+      purrr::set_names(
+        c(names(df)[1:3],
+          paste0(
+            rep(c("special_alert", "alert"), length(names(df)[4:ncol(df)]) / 2),
+            "_",
+            year,
+            "/",
+            names(df)[4:ncol(df)],
+            " ",
+            paste0(5, ":00:00")
+          )))
+    df <- 
+      df[, -c(2,3)] |> 
+      dplyr::slice(-1) |>  
+      tidyr::pivot_longer(-1, 
+                          names_to = c(".value", "datetime"),
+                          names_pattern = "(.+)_(.+)") |> 
+      readr::type_convert("cccc")
+  }
   df |> 
-    purrr::set_names(
-      c(names(df)[1:2],
-        paste0(
-          year,
-          "/",
-          names(df)[3:ncol(df)],
-          " ",
-          paste0(c(5, 17), ":00:00")
-        ))
-    ) |> 
-    dplyr::slice(-1) |>  
-    tidyr::pivot_longer(cols = contains("/"),
-                        names_to = "datetime",
-                        values_to = "alert") |> 
-    readr::type_convert("cdcc") |> 
-    dplyr::mutate(datetime = lubridate::as_datetime(datetime),
-                  alert = dplyr::if_else(alert == "\u25cf",
-                                         TRUE,
-                                         FALSE))
+    dplyr::mutate(datetime = lubridate::as_datetime(datetime)) |> 
+    dplyr::mutate(dplyr::across(tidyselect::contains("alert"), 
+                                ~ dplyr::if_else(. == "\u25cf",
+                                                 TRUE,
+                                                 FALSE)))
 }

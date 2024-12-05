e8954b4f40672866406b34e9422472f705757160
Shinya Uryu <uryu.shinya@tokushima-u.ac.jp>
2024-12-06 03:46:35 +0900

    [modify]`年齢区分：不明`を除いた読み込み

--- R/read_fdma_heatstroke.R
+++ R/read_fdma_heatstroke.R
@@ -31,6 +31,30 @@ read_fdma_heatstroke <- function(path, sheets = NULL, nest = TRUE) {
               tidyr::nest(age_class   = tidyselect::starts_with("年齢区分"),
                           status_type = tidyselect::starts_with("傷病程度"))
           }
+        } else if (ncols == 21L) {
+          df <-
+            readxl::read_xlsx(path, 
+                              sheet = 1,
+                              skip  = 1,
+                              col_names = c("日付",
+                                            "都道府県コード",
+                                            "搬送人員（計）", 
+                                            paste("年齢区分",
+                                                  c("新生児", "乳幼児", "少年",
+                                                    "成人", "高齢者"),
+                                                  sep = "："),
+                                            paste("傷病程度",
+                                                  c("死亡", "重症", "中等症", 
+                                                    "軽症", "その他"),
+                                                  sep = "："),
+                                            paste("発生場所",
+                                                  c("住居", "仕事場\u2460", "仕事場\u2461",
+                                                    "教育機関", "公衆(屋内)", 
+                                                    "公衆(屋外)", "道路", "その他"),
+                                                  sep = "：")),
+                              col_types = c("date",
+                                            "text",
+                                            rep("numeric", 19)))
         } else if (ncols == 22L) {
           df <-
             readxl::read_xlsx(path, 
